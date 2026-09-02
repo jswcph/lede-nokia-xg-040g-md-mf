4a1a9ef01dcffc47b06e03241c82324a44d2aaa1
jswcph <84085592+jswcph@users.noreply.github.com>
2026-09-02 23:45:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,22 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+
+# 如果 an7581.mk 中没有 nokia_xg-040g-md-ubi，则自动追加定义
+cat >> target/linux/airoha/image/an7581.mk << 'EOF'
+
+define Device/nokia_xg-040g-md-ubi
+  $(call Device/FitImageLzma)
+  $(call Device/nokia_xg-040g-md-common)
+  DEVICE_VENDOR := Nokia
+  DEVICE_MODEL := XG-040G-MD (UBI)
+  DEVICE_DTS := an7581-nokia_xg-040g-md
+  DEVICE_DTS_CONFIG := config@1
+  IMAGE_SIZE := 131968k
+  KERNEL_SIZE := 8192k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.itb := append-kernel | pad-to 8192k | append-ubi | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += nokia_xg-040g-md-ubi
+EOF
